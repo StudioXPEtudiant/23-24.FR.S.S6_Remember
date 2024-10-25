3379fff3489057f4e733362096f94bc29fdbfcd6
hudu97 <hugo.moutarlier.layrac@gmail.com>
2024-10-25 20:20:04 +0200

    Caméra suit le joueur

--- Remember/Assets/Resources/Scripts/PlayerMovement.cs
+++ Remember/Assets/Resources/Scripts/PlayerMovement.cs
@@ -15,13 +15,15 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     [SerializeField] private Animator animator;
 
+    [Header("Vitesse du joueur")]
     [SerializeField] private float _walkSpeed = 4f; // Vitesse normale
     [SerializeField] private float _sprintSpeed = 8f; // Vitesse de sprint
-
     private float currentSpeed; // Stocker la vitesse actuelle
 
     private float lastMoveX = 0;
     private float lastMoveY = -1;
+
+    private bool isAttacking = false;
 
     private void Awake()
     {
@@ -54,26 +56,41 @@ public class PlayerMovement : MonoBehaviour
         _defaultPlayerActions.Player.Fire.Disable();
     }
 
-    private void OnFire(InputAction.CallbackContext context)
-    {
-        //  Code pour les attaques
-    }
-
     private void FixedUpdate()
     {
         Vector2 moveDir = _moveAction.ReadValue<Vector2>(); // Lire les valeurs de direction
         
-        if(_sprintAction.IsPressed())
+        if(!isAttacking)
         {
-            currentSpeed = _sprintSpeed; // Active la vitesse de sprint
-            animator.SetBool("IsSprinting", moveDir != Vector2.zero);
-            animator.SetBool("IsWalking", false);
-        }
-        else
-        {
-            currentSpeed = _walkSpeed; // Revenir à la vitesse normale
-            animator.SetBool("IsWalking", moveDir != Vector2.zero);
-            animator.SetBool("IsSprinting", false);
+            if (_sprintAction.IsPressed())
+            {
+                currentSpeed = _sprintSpeed; // Active la vitesse de sprint
+                animator.SetBool("IsSprinting", moveDir != Vector2.zero);
+                animator.SetBool("IsWalking", false);
+            }
+            else
+            {
+                currentSpeed = _walkSpeed; // Revenir à la vitesse normale
+                animator.SetBool("IsWalking", moveDir != Vector2.zero);
+                animator.SetBool("IsSprinting", false);
+            }
+
+
+            animator.SetFloat("MoveX", moveDir.x);
+            animator.SetFloat("MoveY", moveDir.y);
+
+            if (moveDir != Vector2.zero)
+            {
+                lastMoveX = moveDir.x;
+                lastMoveY = moveDir.y;
+            }
+
+
+            if (moveDir == Vector2.zero)
+            {
+                animator.SetFloat("LastMoveX", lastMoveX);
+                animator.SetFloat("LastMoveY", lastMoveY);
+            }
         }
 
         // Aplliquer la vitesse au personnage
@@ -82,22 +99,26 @@ public class PlayerMovement : MonoBehaviour
         vel.y = currentSpeed * moveDir.y;
         rb.velocity = vel;
 
-        if(moveDir != Vector2.zero)
-        {
-            lastMoveX = moveDir.x;
-            lastMoveY = moveDir.y;
-        }
+        Vector2 lookDir = _moveAction.ReadValue<Vector2>();
+        // Debug.Log($"move: {lookDir}");
+    }
 
-        animator.SetFloat("MoveX", moveDir.x);
-        animator.SetFloat("MoveY", moveDir.y);
-
-        if (moveDir == Vector2.zero)
+    private void OnFire(InputAction.CallbackContext context)
+    {
+        if (!isAttacking && !animator.GetBool("IsSprinting"))
         {
+            isAttacking = true;
+            animator.SetBool("IsAttacking", true); // Déclence l'animation d'attaque
+
+            // On passe la direction de la dernière attaque à l'Animator
             animator.SetFloat("LastMoveX", lastMoveX);
             animator.SetFloat("LastMoveY", lastMoveY);
         }
+    }
 
-        Vector2 lookDir = _moveAction.ReadValue<Vector2>();
-        Debug.Log($"move: {lookDir}");
+    public void EndAttack()
+    {
+        animator.SetBool("IsAttacking", false);
     }
 }
+
